acf7b50bebe6953d0b0f38dcc052c879149aab6f
Daniel Yepez Garces <git@dyepezg.dev>
2026-08-06 01:39:34 -0500

    Add meta charset utf-8 and explicit UTF-8 MIME type for HTML diff rendering

--- src/huggle_ui/webkit/huggleweb.cpp
+++ src/huggle_ui/webkit/huggleweb.cpp
@@ -33,7 +33,7 @@ void HuggleWeb::DisplayPage(const QString &url)
 
 void HuggleWeb::RenderHtml(const QString &html)
 {
-    this->ui->webView->setContent(html.toUtf8());
+    this->ui->webView->setContent(html.toUtf8(), "text/html; charset=utf-8");
 }
 
 void HuggleWeb::Click(const QUrl &page)
